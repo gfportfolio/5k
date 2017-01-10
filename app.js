1fd19c37df167df2e905a0d2ac18378e37a42d6c
LEAVITT\gavin-farnsworth <gavin-farnsworth@leavitt.com>
2017-01-10 12:03:52 -0700

    Added Polymer with Typescript

---
+++ app.js
@@ -0,0 +1,11 @@
+/// <reference path="typings/auto.d.ts" />
+System.register(['elements/app-frontend'], function(exports_1, context_1) {
+    "use strict";
+    var __moduleName = context_1 && context_1.id;
+    return {
+        setters:[
+            function (_1) {}],
+        execute: function() {
+        }
+    }
+});
